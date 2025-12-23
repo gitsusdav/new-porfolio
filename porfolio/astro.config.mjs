9fe4f994f9069dc78c8be4dce3bd33f63201a508
gitsusdav <114825973+gitsusdav@users.noreply.github.com>
2025-12-22 22:33:29 -0400

    deploy: gh pages

--- porfolio/astro.config.mjs
+++ porfolio/astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gitsusdav.github.io',
+  base: '/new-porfolio',
   vite: {
     plugins: [tailwindcss()],
   },
